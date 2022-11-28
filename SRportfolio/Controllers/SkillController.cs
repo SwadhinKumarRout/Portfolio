0b8e8ed67035cc37ef04306486f9ad54879a1710
Swadhin Kumar Rout <swadhin.rout@gyansys.com>
2022-11-28 13:09:04 +0530

    first commit

---
+++ SRportfolio/Controllers/SkillController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SRportfolio.Controllers
+{
+    public class SkillController : Controller
+    {
+        // GET: Skill
+        public ActionResult Skill()
+        {
+            ViewBag.ClientSide = "React JS";
+            ViewBag.BackendSide = "Node JS";
+            ViewBag.Database = "SQL Server";
+            return View();
+        }
+    }
+}
